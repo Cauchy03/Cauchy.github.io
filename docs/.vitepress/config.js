615e256fa052960d627daec34af3ba218b1fd853
Cauchy03 <1481856337@qq.com>
2023-06-08 23:18:20 +0800

    feat: add hand written code

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -48,6 +48,7 @@ export default defineConfig({
             { text: 'TS', link: '/guide/TypeScript' },
             { text: 'Project', link: '/guide/Project' },
             { text: '工程化', link: '/guide/工程化' },
+            { text: '手写代码', link: '/guide/手写代码' },
           ],
           collapsible: true, // 是否折叠
           collapsed: false // 是否可折叠
